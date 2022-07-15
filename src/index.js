a082e457dfb0cf647014ad266319c148817e7c8f
ndhieu2701 <hieu@v7rx3.onmicrosoft.com>
2022-07-15 11:13:06 +0700

    update code

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const express = require('express')
+const handlebars = require('express-handlebars')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+// http logger
+app.use(morgan("combined"))
+// template engine 
+app.engine('hbs', handlebars.engine({
+  extname:'hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
